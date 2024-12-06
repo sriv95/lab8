a2f9138e37e3806c95c6e44720e36e6c3878cf0b
sriv95 <srivichai95@gmail.com>
2024-12-06 14:49:11 +0700

    lab8_1.cpp complete

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -6,12 +6,16 @@ int main()
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
+  //   case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
+  //   case 'A': cout << "You have received 5 gems.\n";
+  //   case 'B': cout << "You have received 1 gems.\n";
+  //   case 'C': cout << "You have received 2000 coins.\n";
+  //   case 'D': cout << "You have received very KAK items.\n";
+  // }
+  if(rank=='S')cout << "You have received Super Ultra Rare Unit!!!\n";
+  if(rank<='A'||rank=='S')cout << "You have received 5 gems.\n";
+  if(rank<='B'||rank=='S')cout << "You have received 1 gems.\n";
+  if(rank<='C'||rank=='S')cout << "You have received 2000 coins.\n";
+  if(rank<='D'||rank=='S')cout << "You have received very KAK items.\n";
   return 0;
 }
